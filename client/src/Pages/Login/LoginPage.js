1429b76003cf1eaa7a2e859d8b6264b375b0a72f
Wengsing Wong <invisible734@yahoo.com>
2021-05-07 17:56:52 -0400

    Allon + Login + SignUp

--- client/src/Pages/Login/LoginPage.js
+++ client/src/Pages/Login/LoginPage.js
@@ -3,7 +3,28 @@ import './Login.css'
 
 const LoginPage = () => {
     return (
-        <div className='login-text'>Login Page</div>
+        <div className="form-homepage">
+            <div className="form-container">
+                <h1 className="form-header">Sign Into Carbs4Cards</h1>
+                <form id="login-form">
+
+                    <div className="form-field">
+                        <label for="emailfield">Email</label>
+                        <input id="emailfield" type="text" name="email" placeholder="johndoe@example.com" required />
+                    </div>
+
+                    <div className="form-field">
+                        <label for="passwordfield">Password</label>
+                        <input id="passwordfield" type="password" name="password" placeholder="********" required />
+                    </div>
+                    
+                    <div className="form-field">
+                        <input type="submit" value="Log In" />
+                        <a href="./signup" target="_self" id="signup-link">Sign up here!</a>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
--- client/src/Pages/SignUp/SignUpPage.js
+++ client/src/Pages/SignUp/SignUpPage.js
@@ -1,32 +1,44 @@
-import React from 'react'
+import React from 'react';
+import "../Login/Login.css"
 
-
-const SignUpPage = (props) => {
-    const [timer, setTimer] = React.useState(props.time);
-    const id =React.useRef(null);
-    const clear=()=>{
-    window.clearInterval(id.current)
-    }
-    React.useEffect(()=>{
-       id.current=window.setInterval(()=>{
-        setTimer((time)=>time-1)
-      },1000)
-      return ()=>clear();
-    },[])
-  
-    React.useEffect(()=>{
-      if(timer===0){
-        clear()
-      }
-  
-    },[timer])
+const SignUpPage = () => {
     return (
-        <div className="timer">
-    
-         <div>Time left : {timer} </div>
-    
+        <div className="form-homepage">
+            <div className="form-container">
+                <h1 className="form-header">Sign Up For Carbs4Cards</h1>
+                <form id="signup-form">
+
+                    <p className="signup-p">
+                        Sign up for Carbs4Cards and start working out today!
+                    </p>
+
+                    <div className="form-field">
+                        <label for="emailfield">Email</label>
+                        <input id="emailfield" type="text" name="email" placeholder="johndoe@example.com" required />
+                    </div>
+
+                    <div className="form-field">
+                        <label for="fnamefield">First Name</label>
+                        <input id="fnamefield" type="text" name="fname" placeholder="John" required />
+                    </div>
+
+                    <div className="form-field">
+                        <label for="lnamefield">Last Name</label>
+                        <input id="lnamefield" type="text" name="lname" placeholder="Doe" required />
+                    </div>                
+
+                    <div className="form-field">
+                        <label for="passwordfield">Password</label>
+                        <input id="passwordfield" type="password" name="password" placeholder="********" required />
+                    </div>
+                    
+                    <div className="form-field">
+                        <input type="submit" value="Sign Up" />
+                    </div>
+                </form>
+            </div>
         </div>
-      );
+    );
 }
 
 export default SignUpPage;
